0619bd5939a961c31909da6329646f03b8cfccf8
Amitabh Kale <amitabhkale@gmail.com>
2024-03-14 12:31:06 +0530

    Rendering messages on UI

--- public/js/chat.js
+++ public/js/chat.js
@@ -2,14 +2,34 @@ const socket = io();
 
 socket.on("message", (msg) => {
   console.log(`New Message:  ${msg}`);
+  outputMessage(msg);
 });
 
-document.querySelector("#form").addEventListener("submit", (e) => {
+document.querySelector("#message_form").addEventListener("submit", (e) => {
   e.preventDefault();
 
-  const msg = e.target.elements.message.value;
+  const msg = e.target.elements.message_input.value;
 
-  e.target.elements.message.focus();
+  e.target.elements.message_input.focus();
 
   socket.emit("sendMessage", msg);
 });
+
+function outputMessage(message) {
+  const testUser = {
+    name: "John",
+    text: "Blh blah blah",
+    createdAt: "9:12pm",
+  };
+
+  const div = document.createElement("div");
+  div.className = "chat-message";
+  div.innerHTML = `<p>
+  <span class="message-user">${testUser.name}</span>
+  <span class="message-date">${testUser.createdAt}</span>
+  <p>
+  <p>${message}</p>
+  `;
+
+  document.getElementById("messages").appendChild(div);
+}
